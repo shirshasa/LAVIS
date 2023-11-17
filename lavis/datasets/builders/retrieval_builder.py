6dcaf114962d4cf02cd3eb52fa00b37af80235cb
Katerina <katerinagorbac@gmail.com>
2023-11-17 14:57:26 +0300

    add scripts to train img2text retrival on custom dataset
    - fix issues if no ddp is used during training
    - added custom dataset based on ImageFolder format
    - added custom dataset config, dataset builder
    - added classification report for evaluation
    - updated project config

--- lavis/datasets/builders/retrieval_builder.py
+++ lavis/datasets/builders/retrieval_builder.py
@@ -4,8 +4,11 @@
  SPDX-License-Identifier: BSD-3-Clause
  For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 """
+import os
+import warnings
 
 from lavis.datasets.builders.base_dataset_builder import BaseDatasetBuilder
+from lavis.datasets.datasets.custom_dataset import CustomDataset, CustomEvalDataset
 from lavis.datasets.datasets.retrieval_datasets import (
     RetrievalDataset,
     RetrievalEvalDataset,
@@ -38,6 +41,71 @@ class COCORetrievalBuilder(BaseDatasetBuilder):
     eval_dataset_cls = RetrievalEvalDataset
 
     DATASET_CONFIG_DICT = {"default": "configs/datasets/coco/defaults_ret.yaml"}
+
+
+@registry.register_builder("custom_retrieval")
+class CustomRetrievalBuilder(BaseDatasetBuilder):
+    train_dataset_cls = CustomDataset
+    eval_dataset_cls = CustomEvalDataset
+
+    DATASET_CONFIG_DICT = {"default": "configs/datasets/custom_ret.yaml"}
+
+    CLASSES = [
+        "cat",
+        "dog",
+    ]
+
+    def _download_data(self):
+        pass
+
+    def build(self):
+        """
+        Create by split datasets inheriting torch.utils.data.Datasets.
+
+        # build() can be dataset-specific. Overwrite to customize.
+        """
+        self.build_processors()
+
+        build_info = self.config.build_info
+
+        ann_info = build_info.annotations
+
+        datasets = dict()
+        for split in ann_info.keys():
+            if split not in ["train", "val", "test"]:
+                continue
+
+            is_train = split == "train"
+
+            # processors
+            vis_processor = (
+                self.vis_processors["train"]
+                if is_train
+                else self.vis_processors["eval"]
+            )
+            text_processor = (
+                self.text_processors["train"]
+                if is_train
+                else self.text_processors["eval"]
+            )
+
+            # annotation path
+            ann_paths = ann_info.get(split).storage
+            assert isinstance(ann_paths, str)
+
+            if not os.path.exists(ann_paths):
+                warnings.warn("storage path {} does not exist.".format(ann_paths))
+
+            # create datasets
+            dataset_cls = self.train_dataset_cls if is_train else self.eval_dataset_cls
+            datasets[split] = dataset_cls(
+                vis_processor=vis_processor,
+                text_processor=text_processor,
+                img_folder=ann_paths,
+                classes=self.CLASSES
+            )
+
+        return datasets
 
 
 @registry.register_builder("flickr30k")
--- lavis/tasks/retrieval.py
+++ lavis/tasks/retrieval.py
@@ -11,6 +11,8 @@ import os
 
 import numpy as np
 import torch
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score, classification_report, confusion_matrix
+
 from lavis.common.dist_utils import is_main_process
 from lavis.common.registry import registry
 from lavis.tasks.base_task import BaseTask
@@ -41,6 +43,11 @@ class RetrievalTask(BaseTask):
                 data_loader.dataset.img2txt,
             )
             logging.info(eval_result)
+
+            self._report_classification_results(
+                score_i2t, data_loader.dataset.img2txt, data_loader.dataset.classnames
+            )
+
         else:
             eval_result = None
 
@@ -67,37 +74,37 @@ class RetrievalTask(BaseTask):
 
         # Compute metrics
         tr1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
-        tr5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
-        tr10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+        tr2 = 100.0 * len(np.where(ranks < 2)[0]) / len(ranks)
+        tr3 = 100.0 * len(np.where(ranks < 3)[0]) / len(ranks)
 
         # Text->Images
-        ranks = np.zeros(scores_t2i.shape[0])
+        # ranks = np.zeros(scores_t2i.shape[0])
+        #
+        # for index, score in enumerate(scores_t2i):
+        #     inds = np.argsort(score)[::-1]
+        #     ranks[index] = np.where(inds == txt2img[index])[0][0]
+        #
+        # # Compute metrics
+        # ir1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+        # ir5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+        # ir10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+        #
+        tr_mean = (tr1 + tr2 + tr3) / 3
+        # ir_mean = (ir1 + ir5 + ir10) / 3
+        # r_mean = (tr_mean + ir_mean) / 2
 
-        for index, score in enumerate(scores_t2i):
-            inds = np.argsort(score)[::-1]
-            ranks[index] = np.where(inds == txt2img[index])[0][0]
-
-        # Compute metrics
-        ir1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
-        ir5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
-        ir10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
-
-        tr_mean = (tr1 + tr5 + tr10) / 3
-        ir_mean = (ir1 + ir5 + ir10) / 3
-        r_mean = (tr_mean + ir_mean) / 2
-
-        agg_metrics = (tr1 + tr5 + tr10) / 3
+        agg_metrics = (tr1 + tr2 + tr3) / 3
 
         eval_result = {
             "txt_r1": tr1,
-            "txt_r5": tr5,
-            "txt_r10": tr10,
+            "txt_r5": tr2,
+            "txt_r10": tr3,
             "txt_r_mean": tr_mean,
-            "img_r1": ir1,
-            "img_r5": ir5,
-            "img_r10": ir10,
-            "img_r_mean": ir_mean,
-            "r_mean": r_mean,
+            # "img_r1": ir1,
+            # "img_r5": ir5,
+            # "img_r10": ir10,
+            # "img_r_mean": ir_mean,
+            # "r_mean": r_mean,
             "agg_metrics": agg_metrics,
         }
         with open(
@@ -105,3 +112,34 @@ class RetrievalTask(BaseTask):
         ) as f:
             f.write(json.dumps(eval_result) + "\n")
         return eval_result
+
+    @torch.no_grad()
+    def _report_classification_results(self, scores_i2t, img2txt, classes):
+        y_pred = []
+        y_true = []
+
+        for index, score in enumerate(scores_i2t):
+            ind_max_score = np.argmax(score)
+            true_text_ind = img2txt[index][0]  # TODO
+            y_pred.append(classes[ind_max_score])
+            y_true.append(classes[true_text_ind])
+
+        precision, recall, fscore, _ = precision_recall_fscore_support(
+            y_true, y_pred, average="weighted", zero_division=0
+        )
+        accuracy = accuracy_score(y_pred=y_pred, y_true=y_true)
+
+        clf_report = classification_report(y_pred=y_pred, y_true=y_true)
+
+        logging.info(f"F1 score: {fscore:.2f}")
+        logging.info(f"Accuracy: {accuracy:.2f}")
+        logging.info(f"\n{clf_report}\n")
+        logging.info(f"\n{confusion_matrix(y_pred=y_pred, y_true=y_true)}\n")
+
+        with open(
+            os.path.join(registry.get_path("output_dir"), "evaluate.txt"), "a"
+        ) as f:
+            f.write(f"F1 score: {fscore:.2f}" + "\n")
+            f.write(f"Accuracy: {accuracy:.2f}")
+            f.write(f"\n{clf_report}\n")
+            f.write(f"\n{confusion_matrix(y_pred=y_pred, y_true=y_true)}\n")
